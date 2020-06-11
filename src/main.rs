dff070c17525dd973f7d5b92fee488cb7b330675
Colin Woodbury <colin@fosskers.ca>
2020-06-11 13:06:34 -0700

    Basic CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+//! A tool for measuring repository contributions.
+
+use structopt::StructOpt;
+
+//- ~credit~: Just pull as much as possible via the Github API.
+//- Who comments the most?
+//- Average time to first issue response?
+//- Average response time from Owner?
+//- Who's PRs are getting merged?
+//- Who is reviewing?
+//- Query multiple repos at once and merge the results
+//- Gotta call the endpoints I want manually.
+
+// Number of commits on `master` isn't counted - you can see that on Github :)
+
+#[derive(StructOpt)]
+#[structopt(about = "A tool for measuring repository contributions")]
+struct Env {
+    /// Github personal access token
+    #[structopt(long)]
+    token: String,
+
+    /// A Github repository to check
+    #[structopt(name = "REPO")]
+    repos: Vec<String>,
+
+    /// Output as JSON
+    #[structopt(short, long)]
+    json: bool,
+}
+
 fn main() {
+    let env = Env::from_args();
+
     println!("Hello, world!");
 }
